18772a86bdf56e42711fc8b0e7935b7d259b516b
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-05-05 16:31:36 +0200

    fix

--- src/org/snakcs/Snack8.java
+++ src/org/snakcs/Snack8.java
@@ -9,13 +9,15 @@ public class Snack8 {
 		
 		Integer[] evenArr = new Integer[10];
 		Integer[] oddArr = new Integer[10];
+		int evenCount = 0;
+		int oddCount = 0;
 		for(int i = 0; i<10;i++) {
 			int randNum = rnd.nextInt(101);
 			
 			if(randNum % 2 == 0) {
-				evenArr[i] = randNum;
+				evenArr[evenCount++] = randNum;
 			}else {
-				oddArr[i] = randNum;
+				oddArr[oddCount++] = randNum;
 			}
 			
 		}
