9686ac6c9f82a6d8bc8309b6a38233d1fa7f7d5a
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-05-05 11:23:52 +0200

    snack 3 done

--- src/org/snakcs/Snack3.java
+++ src/org/snakcs/Snack3.java
@@ -2,6 +2,16 @@ package org.snakcs;
 
 public class Snack3 {
 	public static void main(String[] args) {
-		int[] numbers = {1, 2, 3, 4, 5};
+		int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+		int sum = 0;
+		
+		for (int i = 0; i < numbers.length; i++) {
+			if(numbers[i] % 2 == 0) {
+				sum += numbers[i];
+				System.out.println(numbers[i]);
+			}
+		}
+		System.out.println(sum);
 	}
 }
